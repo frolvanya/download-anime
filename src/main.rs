e68adf19930c2dca2306d13dff472085bc593cf6
Ivan <frolik@Vlads-MacBook-Pro.local>
2022-08-16 20:29:45 +0200

    feat: working multithreaded anime downloader

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-use core::time;
-use std::io::Write;
+use std::thread::{self, JoinHandle};
 
 use reqwest::blocking::Client;
 use reqwest::header;
 
 use select::document::Document;
 use select::predicate::Name;
-
-use std::sync::mpsc::{channel, Receiver, Sender};
-use std::thread;
 
 use clap::Parser;
 
@@ -27,6 +23,10 @@ struct Args {
     /// Specifies episodes resolution
     #[clap(short, long, value_parser, default_value_t = 1080)]
     resolution: u16,
+
+    /// Specifies concurrency
+    #[clap(short, long, value_parser, default_value_t = 12)]
+    concurrency: u16,
 }
 
 fn if_episode_exists(client: Client, url: String) -> bool {
@@ -56,20 +56,24 @@ fn get_video_urls(client: Client, url: String) -> Vec<String> {
         .collect::<Vec<String>>()
 }
 
-fn download_episode(client: Client, send: Sender<bool>, url: String, anime: String, episode: u16) {
-    let mut response = client.get(url).send().expect("Wasn't able to send request");
+fn download_episode(client: Client, url: String, anime: String, episode: u16) {
+    let mut response = client
+        .get(url.clone())
+        .send()
+        .expect("Wasn't able to send request");
+
+    if !response.status().is_success() {
+        println!("Bad response status! Trying again...");
+        download_episode(client, url, anime.clone(), episode);
+    }
 
     match std::fs::File::create(format!("{}/episode-{}.mp4", anime, episode)) {
         Ok(mut file) => match std::io::copy(&mut response, &mut file) {
             Ok(_) => {
-                send.send(true)
-                    .expect("An error occurred while sending the signal");
-                std::thread::sleep(time::Duration::from_secs(1));
+                println!("Episode {}: ✔", episode);
             }
             Err(err) => {
-                send.send(false)
-                    .expect("An error occurred while sending the signal");
-                std::thread::sleep(time::Duration::from_secs(1));
+                println!("Episode {}: ✘", episode);
 
                 println!(
                     "Wasn't able to save `episode-{}.mp4` due to {}",
@@ -78,9 +82,7 @@ fn download_episode(client: Client, send: Sender<bool>, url: String, anime: Stri
             }
         },
         Err(err) => {
-            send.send(false)
-                .expect("An error occurred while sending the signal");
-            std::thread::sleep(time::Duration::from_secs(1));
+            println!("Episode {}: ✘", episode);
 
             println!(
                 "Wasn't able to save `episode-{}.mp4` due to {}",
@@ -90,33 +92,13 @@ fn download_episode(client: Client, send: Sender<bool>, url: String, anime: Stri
     };
 }
 
-fn loading_animation(episode: u16, recv: Receiver<bool>) {
-    let frames = ["|", "/", "-", "\\"];
-    print!("Episode {}:  ", episode);
-
-    loop {
-        for frame in frames {
-            match recv.try_recv() {
-                Ok(true) => {
-                    println!("{}✔", 8u8 as char);
-                    return;
-                }
-                Ok(false) => {
-                    println!("{}✘", 8u8 as char);
-                    return;
-                }
-                Err(_) => {}
-            };
-
-            print!("{}{}", 8u8 as char, frame);
-            std::io::stdout().flush().unwrap();
-            std::thread::sleep(time::Duration::from_millis(100));
-        }
+fn wait_for_threads(threads: Vec<JoinHandle<()>>) {
+    for child_thread in threads {
+        child_thread.join().unwrap();
     }
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let mut headers = header::HeaderMap::new();
     headers.insert(header::USER_AGENT, header::HeaderValue::from_static("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Safari/605.1.15"));
     let client = reqwest::blocking::Client::builder()
@@ -129,7 +111,7 @@ async fn main() {
     let anime = args.anime;
     if !if_episode_exists(
         client.clone(),
-        format!("https://jut.su/{}/episode-1.html", anime.clone()),
+        format!("https://jut.su/{}/episode-1.html", anime),
     ) {
         panic!("This anime doesn't exists");
     };
@@ -142,9 +124,7 @@ async fn main() {
                 .map(|x| x.parse::<u16>().unwrap())
                 .collect::<Vec<u16>>();
             if range[0] > range[1] {
-                let tmp_right_range = range[0];
-                range[0] = range[1];
-                range[1] = tmp_right_range;
+                range.swap(0, 1);
             }
 
             range[0]..range[1] + 1
@@ -159,27 +139,37 @@ async fn main() {
         _ => panic!("Wrong resolution. Please, pick one of these: 360, 480, 720, 1080"),
     };
 
+    let concurrency = args.resolution;
+
     match std::fs::create_dir(anime.clone()) {
-        Err(_) => println!("`{}` folder is already exists", anime.clone()),
-        Ok(_) => println!("Created `{}` folder", anime.clone()),
+        Err(_) => println!("`{}` folder is already exists", anime),
+        Ok(_) => println!("Created `{}` folder", anime),
     }
 
+    let mut threads = Vec::new();
     for episode in episodes {
-        let (send, recv) = channel();
-
         let url = format!("https://jut.su/{}/episode-{}.html", anime.clone(), episode);
         if !if_episode_exists(client.clone(), url.clone()) {
             break;
         }
 
         let video_urls = get_video_urls(client.clone(), url.clone());
-        thread::spawn(move || loading_animation(episode, recv));
-        download_episode(
-            client.clone(),
-            send,
-            video_urls[resolution].clone(),
-            anime.clone(),
-            episode,
-        );
+        let (client_clone, anime_clone) = (client.clone(), anime.clone());
+
+        threads.push(thread::spawn(move || {
+            download_episode(
+                client_clone,
+                video_urls[resolution].clone(),
+                anime_clone,
+                episode,
+            );
+        }));
+
+        if episode % concurrency == 0 {
+            wait_for_threads(threads);
+            threads = Vec::new();
+        }
     }
+
+    wait_for_threads(threads);
 }
